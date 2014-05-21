2bd8bcacc32fda1028dc507e3a5b329f91f725b1
David G Chaves <davidgchaves@gmail.com>
2014-05-21 10:58:19 +0200

    Create initial step definitions for Valet Parking feature

---
+++ features/step_definitions/valet_steps.rb
@@ -0,0 +1,7 @@
+When /^I park my car in the Valet Parking Lot for (.*)$/ do |duration|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I will have to pay (.*)$/ do |price|
+  pending # express the regexp above with the code you wish you had
+end
