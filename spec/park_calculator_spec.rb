12bf818b58b7ac0fdbd4c7a54a98a1c09f3f1eef
David G Chaves <davidgchaves@gmail.com>
2014-06-12 09:11:50 +0200

    Transformation Priority Premise for Economy ParkCalculator - Step 13b   - 'Parking in the Economy Parking Lot' context   - It 'costs 54$ for 1 week'

--- spec/park_calculator_spec.rb
+++ spec/park_calculator_spec.rb
@@ -176,5 +176,10 @@ describe ParkCalculator do
       park_calculator = ParkCalculator.new economy, 6*24*60 + 1
       expect(park_calculator.estimated_cost).to eq 54
     end
+
+    it "costs 54$ for 1 week" do
+      park_calculator = ParkCalculator.new economy, 7*24*60
+      expect(park_calculator.estimated_cost).to eq 54
+    end
   end
 end
