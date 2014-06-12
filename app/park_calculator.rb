57b397f05bffd6cfd5e8b1378073568412cdf631
David G Chaves <davidgchaves@gmail.com>
2014-06-12 09:25:08 +0200

    Transformation Priority Premise for Economy ParkCalculator - Step 14b   - 'Parking in the Economy Parking Lot' context   - It 'costs 56$ for 1 week and 1 minute'   - I think I'm there :)

--- app/park_calculator.rb
+++ app/park_calculator.rb
@@ -16,7 +16,7 @@ class ParkCalculator
     elsif parking == Parkings.short_term
       ShortTimeParkingCalculator.calculate_cost duration
     else parking == Parkings.economy
-      duration > SIX_DAYS ? DISCOUNTED_WEEK_COST : (DISCOUNTED_DAY_COST * full_days_parked(duration)) + last_day_cost(last_day_duration(duration))
+      (DISCOUNTED_WEEK_COST * weeks_parked(duration)) + last_week_cost(last_week_duration(duration))
     end
   end
 
@@ -27,24 +27,37 @@ class ParkCalculator
   FOUR_HOURS = 4 * AN_HOUR
   A_DAY = 24 * AN_HOUR
   SIX_DAYS = 6 * A_DAY
+  A_WEEK = 7 * A_DAY
 
   HOUR_COST = 2
   DISCOUNTED_DAY_COST = 9
   DISCOUNTED_WEEK_COST = 54
 
-  def full_days_parked(duration)
+  def weeks_parked(duration)
+    (duration - A_MINUTE) / A_WEEK
+  end
+
+  def days_parked(duration)
     (duration - A_MINUTE) / A_DAY
+  end
+
+  def hours_parked(duration)
+    ((duration - A_MINUTE) / AN_HOUR)
+  end
+
+  def last_week_cost(duration)
+    duration > SIX_DAYS ? DISCOUNTED_WEEK_COST : (DISCOUNTED_DAY_COST * days_parked(duration)) + last_day_cost(last_day_duration(duration))
   end
 
   def last_day_cost(duration)
     duration > FOUR_HOURS ? DISCOUNTED_DAY_COST : (hours_parked(duration) + 1) * HOUR_COST
   end
 
-  def last_day_duration(duration)
-    duration - (full_days_parked(duration) * A_DAY)
+  def last_week_duration(duration)
+    duration - (weeks_parked(duration) * A_WEEK)
   end
 
-  def hours_parked(duration)
-    ((duration - A_MINUTE) / AN_HOUR)
+  def last_day_duration(duration)
+    duration - (days_parked(duration) * A_DAY)
   end
 end
