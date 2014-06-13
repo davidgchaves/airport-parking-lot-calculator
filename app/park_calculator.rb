823cf6aeefceb84e86023a48a24f532a571d3e5e
David G Chaves <davidgchaves@gmail.com>
2014-06-13 09:34:19 +0200

    Transformation Priority Premise for Long-Term G ParkCalculator - Step 6   - 'Parking in the Long-Term Garage Parking Lot' context   - It 'costs 12$ for 6 hours and 1 minute'

--- app/park_calculator.rb
+++ app/park_calculator.rb
@@ -20,14 +20,20 @@ class ParkCalculator
     when Parkings.economy
       EconomyParkingCalculator.calculate_cost duration
     when Parkings.long_term_garage
-     (((duration - A_MINUTE) / AN_HOUR) + 1) * HOUR_COST
+      duration > SIX_HOURS ? DISCOUNTED_DAY_COST : (hours_parked(duration) + 1) * HOUR_COST
     end
   end
 
   private
 
   A_MINUTE = 1
-  AN_HOUR = 60
+  AN_HOUR = 60 * A_MINUTE
+  SIX_HOURS = 6 * AN_HOUR
 
   HOUR_COST = 2
+  DISCOUNTED_DAY_COST = 12
+
+  def hours_parked(duration)
+    (duration - A_MINUTE) / AN_HOUR
+  end
 end
--- spec/park_calculator_spec.rb
+++ spec/park_calculator_spec.rb
@@ -225,5 +225,10 @@ describe ParkCalculator do
       park_calculator = ParkCalculator.new long_term_garage, 6*60
       expect(park_calculator.estimated_cost).to eq 12
     end
+
+    it "costs 12$ for 6 hours and 1 minute" do
+      park_calculator = ParkCalculator.new long_term_garage, 6*60 + 1
+      expect(park_calculator.estimated_cost).to eq 12
+    end
   end
 end
