24938ed7eb74e680a58e6e4495f8672540361b46
David G Chaves <davidgchaves@gmail.com>
2014-06-16 10:12:52 +0200

    Long-Term Garage Calculator working

--- app/long_term_parking_calculator.rb
+++ app/long_term_parking_calculator.rb
@@ -1,6 +1,7 @@
 class LongTermParkingCalculator
   def self.calculate_garage_cost(parking, duration)
-    DISCOUNTED_WEEK_COST * weeks_parked(duration) + last_week_cost(last_week_duration(duration))
+    DISCOUNTED_WEEK_COST_FOR_GARAGE * weeks_parked(duration) +
+      last_week_cost(last_week_duration(duration), CUT_DAY_FOR_GARAGE, DISCOUNTED_WEEK_COST_FOR_GARAGE, CUT_HOUR_FOR_GARAGE, DISCOUNTED_DAY_COST_FOR_GARAGE)
   end
 
   def self.calculate_surface_cost(parking, duration)
@@ -17,31 +18,34 @@ class LongTermParkingCalculator
   A_WEEK = 7 * A_DAY
 
   HOUR_COST = 2
-  DISCOUNTED_DAY_COST = 12
-  DISCOUNTED_WEEK_COST = 72
+
+  CUT_HOUR_FOR_GARAGE = SIX_HOURS
+  DISCOUNTED_DAY_COST_FOR_GARAGE = 12
+  CUT_DAY_FOR_GARAGE = SIX_DAYS
+  DISCOUNTED_WEEK_COST_FOR_GARAGE = 72
 
   def self.weeks_parked(duration)
     (duration - A_MINUTE) / A_WEEK
   end
 
-  def self.last_week_cost(duration)
-    duration > SIX_DAYS ? DISCOUNTED_WEEK_COST : produce_week_cost(duration)
+  def self.last_week_cost(duration, cut_day, discounted_week_cost, cut_hour, discounted_day_cost)
+    duration > cut_day ? discounted_week_cost : produce_week_cost(duration, cut_hour, discounted_day_cost)
   end
 
   def self.last_week_duration(duration)
     duration - (weeks_parked(duration) * A_WEEK)
   end
 
-  def self.produce_week_cost(duration)
-    DISCOUNTED_DAY_COST * days_parked(duration) + last_day_cost(last_day_duration(duration))
+  def self.produce_week_cost(duration, cut_hour, discounted_day_cost)
+    discounted_day_cost * days_parked(duration) + last_day_cost(last_day_duration(duration), cut_hour, discounted_day_cost)
   end
 
   def self.days_parked(duration)
     (duration - A_MINUTE) / A_DAY
   end
 
-  def self.last_day_cost(duration)
-    duration > SIX_HOURS ? DISCOUNTED_DAY_COST : produce_day_cost(duration)
+  def self.last_day_cost(duration, cut_hour, discounted_day_cost)
+    duration > cut_hour ? discounted_day_cost : produce_day_cost(duration)
   end
 
   def self.last_day_duration(duration)
